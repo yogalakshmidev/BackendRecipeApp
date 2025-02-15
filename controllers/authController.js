f447a77a177fc46dd92e5ef997ed95502f4b5848
Yogalakshmi <yogarajakanibe@gmail.com>
2025-02-15 15:14:12 +0530

    User Logged in using Jwt token

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,13 @@ const User = require("../models/user");
 // to hide password use bcrypt
 const bcrypt = require('bcrypt');
 
+// import jwt 
+const jwt = require('jsonwebtoken');
+
+// to use env
+require('dotenv').config();
+
+
 const authController = {
   register: async (req, res) => {
     try {
@@ -36,6 +43,30 @@ const authController = {
   },
   login: async (req, res) => {
     try {
+      // Get the details of the user from the request body
+      const {email,password} = req.body;
+
+      // check if the user exist
+      const user = await User.findOne({email});
+
+      // if the user does not exist return error
+      if(!user){
+        return res.status(400).json({message:"User does not exist"});
+      }
+
+      // check if the password is correct
+      const isPasswordCorrect = await bcrypt.compare(password,user.password);
+       if(!isPasswordCorrect){
+        return res.status(400).json({message:"Invalid credentials"});
+       }
+
+      //  Generate a token using jwt
+      const token = jwt.sign({userId:user._id},process.env.JWT_SECRET);
+
+      //  return a success message
+      res.status(200).json({message:"Userlogged in successfully",token});
+
+
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
